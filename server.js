92bb69d3ce29f31c7eec6dac1b422e27b519b124
Isaac <ifrishman89@gmail.com>
2021-01-23 00:10:11 -0600

    update file structure and fix routes

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
+const path = require("path");
+const morgan = require("morgan");
+const db = require("./models");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const workoutsController = require("./controllers/workoutsController");
+// const workoutController = require("./controllers/workoutController");
 
 app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
@@ -18,6 +23,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useCreateIndex: true,
   useFindAndModify: false,
 });
+
+// Route
+app.use(require("./routes/api-routes"));
+
 
 const connection = mongoose.connection;
 
@@ -35,8 +44,18 @@ app.get("/api/config", (req, res) => {
   });
 });
 
-app.use(workoutsController);
-require("./controllers/htmlRoutes")(app);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
